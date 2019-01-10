0899ecf3980800c5bde742b71cf313e0bdb074bd
goodude123 <assasinmcpl@gmail.com>
2019-01-11 00:57:29 +0100

    Added max_length parameter to all CharField objects.

--- game/models.py
+++ game/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Hero(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     level = models.IntegerField()
     experience = models.IntegerField()
     strength = models.IntegerField()
@@ -14,20 +14,20 @@ class Hero(models.Model):
     critic_chance = models.IntegerField()
     health = models.IntegerField()
 
-    def __str__(self);
+    def __str__(self):
         return "Hero " + self.name
 
 
 class Location(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     
-    def __str__(self);
+    def __str__(self):
         return "Location " + self.name
 
 
 class Action(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
-    def __str__(self);
+    def __str__(self):
         return "Action " + self.name
