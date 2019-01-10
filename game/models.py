2f1d3af80273a38bab6aaf927a167b755d745196
goodude123 <assasinmcpl@gmail.com>
2019-01-10 22:39:24 +0100

    Hero model.

--- game/models.py
+++ game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Hero(models.model):
+    name = models.CharField()
+    level = models.IntegerField()
+    experience = models.IntegerField()
+    strength = models.IntegerField()
+    intelligence = models.IntegerField()
+    agility = models.IntegerField()
+    dodge_chance = models.IntegerField()
+    critic_chance = models.IntegerField()
+    health = models.IntegerField()
+
+    def __str__(self);
+        return self.name
