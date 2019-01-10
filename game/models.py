b3d52c57ef9847bbebca83ed58662dcafb2f45c2
goodude123 <assasinmcpl@gmail.com>
2019-01-11 00:43:15 +0100

    Added user as foreign key to hero model.

--- game/models.py
+++ game/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Hero(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField()
     level = models.IntegerField()
     experience = models.IntegerField()
