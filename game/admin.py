4305410b395a8de00ab05423c6acacefb0274fb3
goodude123 <assasinmcpl@gmail.com>
2019-01-14 16:36:59 +0100

    Registered models to admin panel.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Hero)
+admin.site.register(models.Item)
+admin.site.register(models.Location)
+admin.site.register(models.Action)
