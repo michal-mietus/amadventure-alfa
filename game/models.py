3b01301ca7037cad4f607827ba64cdfe47bd8dd3
goodude123 <assasinmcpl@gmail.com>
2019-01-15 16:30:51 +0100

    Added default values to Hero and Statistics fields.

--- game/models.py
+++ game/models.py
@@ -6,9 +6,9 @@ class Statistics(models.Model):
     strength = models.IntegerField()
     intelligence = models.IntegerField()
     agility = models.IntegerField()
-    dodge_chance = models.IntegerField()
-    critic_chance = models.IntegerField()
-    health = models.IntegerField()
+    dodge_chance = models.IntegerField(default=0)
+    critic_chance = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
 
     class Meta:
         abstract = True
@@ -17,8 +17,9 @@ class Statistics(models.Model):
 class Hero(Statistics):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
-    level = models.IntegerField()
-    experience = models.IntegerField()
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+    health = models.IntegerField(default=10)
     
     def __str__(self):
         return "Hero " + self.name
