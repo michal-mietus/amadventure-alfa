3113c697be52788b1e3678fa7dec64d1ef89ce81
goodude123 <assasinmcpl@gmail.com>
2019-01-15 15:58:50 +0100

    Clearer included game paths.

--- amadventure/urls.py
+++ amadventure/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from game.urls import urlpatterns as game_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game/', include(game_urls))
+    path('', include('game.urls'))
 ]
