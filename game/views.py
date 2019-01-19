135d115867ab0117c948fe84fecbfd33353d3669
goodude123 <assasinmcpl@gmail.com>
2019-01-19 01:15:31 +0100

    Added view for upgrade hero. Currently after post data returns NOT NULL constraint fail.

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse
 from django.views.generic.edit import View, FormView
-from .forms import HeroForm
+from .forms import HeroCreateForm, HeroUpgradeForm
+from .models import Hero
 
 
 class MainView(View):
@@ -10,14 +11,37 @@ class MainView(View):
         return render(request, self.template_name)
 
 
-class CreateHeroView(FormView):
+class HeroCreateView(FormView):
     template_name = 'game/create_hero.html'
-    form_class = HeroForm
-
-    def get_success_url(self):
-        return reverse('game:main')
+    form_class = HeroCreateForm
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('game:main')
+
+
+class HeroUpgradeView(FormView):
+    template_name = 'game/upgrade_hero.html'
+    form_class = HeroUpgradeForm
+
+    def get_initial(self):
+        initial = super(HeroUpgradeView, self).get_initial()
+        # TODO how get current hero (if we would have more than one to choose)?
+        hero = Hero.objects.get(user=self.request.user)
+        initial['strength'] = hero.strength
+        initial['intelligence'] = hero.intelligence
+        initial['agility'] = hero.agility
+        initial['vitality'] = hero.vitality
+
+        return initial
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('game:main')
