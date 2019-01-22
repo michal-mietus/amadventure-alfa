b598b5bbc7f9d0bf3a4982ca65bbc809de23eb57
goodude123 <assasinmcpl@gmail.com>
2019-01-22 23:02:03 +0100

    Slideshow next and previous buttons. Somethign is not correct in scrolling images.

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -12,4 +12,14 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
-} 
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
