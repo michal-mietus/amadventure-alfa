f2035bc4ee9825395d7be926f68baad5c76a06b1
goodude123 <assasinmcpl@gmail.com>
2019-01-22 23:04:30 +0100

    Changed back to automatic slideshow.

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -4,22 +4,11 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  console.log('slides', slides);
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
