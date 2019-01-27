ebe2f63ad407a9f438b9c15df452b169dde837f0
goodude123 <assasinmcpl@gmail.com>
2019-01-27 23:38:07 +0100

    Added welcome view to url.

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'game'
 
 urlpatterns = [
-    path('', views.MainView.as_view(), name='main'),
+    path('', views.WelcomeView.as_view(), name='amadventure'),
+    path('main/', views.MainView.as_view(), name='main'),
     path('hero_create/', views.HeroCreateView.as_view(), name='hero_create'),
     path('hero_upgrade/', views.HeroUpgradeView.as_view(), name='hero_upgrade'),
     
