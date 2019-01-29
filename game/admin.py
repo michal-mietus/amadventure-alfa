328914563949abb485742542a4d86b46e55c7275
goodude123 <assasinmcpl@gmail.com>
2019-01-29 20:56:45 +0100

    Registered all new models.

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from . import models
+from .models.hero import Hero
+from .models.item import Item
+from .models.location import Location
+from .models.action import Action
+from .models.statistic import Statistic
 
 
-admin.site.register(models.Hero)
-admin.site.register(models.Item)
-admin.site.register(models.Location)
-admin.site.register(models.Action)
+admin.site.register(Hero)
+admin.site.register(Item)
+admin.site.register(Location)
+admin.site.register(Action)
+admin.site.register(Statistic)
