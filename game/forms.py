09af9d4dc5d35547caf1994b9827684cf876f60e
goodude123 <assasinmcpl@gmail.com>
2019-01-29 20:57:10 +0100

    Changed path to Hero model.

--- game/forms.py
+++ game/forms.py
@@ -1,14 +1,14 @@
 from django.forms import ModelForm
-from . import models
+from .models.hero import Hero
 
 
 class HeroCreateForm(ModelForm):
     class Meta:
-        model = models.Hero
+        model = Hero
         fields = ['name', 'strength', 'intelligence', 'agility', 'vitality']
 
 
 class HeroUpgradeForm(ModelForm):
     class Meta:
-        model = models.Hero
+        model = Hero
         fields = ['strength', 'intelligence', 'agility', 'vitality']
