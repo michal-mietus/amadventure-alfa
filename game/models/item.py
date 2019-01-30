91cf3ae6cffc7a2c427da50da48dab42e88f44b0
goodude123 <assasinmcpl@gmail.com>
2019-01-30 19:53:44 +0100

    New method for getting all item related statistics.

--- game/models/item.py
+++ game/models/item.py
@@ -1,14 +1,15 @@
 from django.db import models
 from .hero import Hero
-from .models import Statistics
 
 
-
-class Item(Statistics):
+class Item(models.Model):
     name = models.CharField(max_length=20)
     owner = models.ForeignKey(Hero, on_delete=models.CASCADE)
     
     def __str__(self):
         repr = "Item " + self.name
         return repr
+
+    def get_all_statistics(self):
+        return self.itemstatistic_set.all()
 
