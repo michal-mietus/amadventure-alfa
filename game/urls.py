1dc9493cd4dcd44e1d8726a57ea2b764698ef9e3
goodude123 <assasinmcpl@gmail.com>
2019-01-30 19:50:22 +0100

    CreateHeroView replaced with new function view.

--- game/urls.py
+++ game/urls.py
@@ -7,7 +7,7 @@ app_name = 'game'
 urlpatterns = [
     path('', views.WelcomeView.as_view(), name='amadventure'),
     path('main/', views.MainView.as_view(), name='main'),
-    path('hero_create/', views.HeroCreateView.as_view(), name='hero_create'),
+    path('create_hero/', views.create_hero, name='create_hero'),
     path('hero_upgrade/', views.HeroUpgradeView.as_view(), name='hero_upgrade'),
     
 ]
