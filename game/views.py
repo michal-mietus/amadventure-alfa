5be57960a379c2381fd7b5ddc7d4a28fd4b3de5c
goodude123 <assasinmcpl@gmail.com>
2019-01-30 19:51:17 +0100

    New imports:   form,   item and hero statistic models New create_hero function view.

--- game/views.py
+++ game/views.py
@@ -3,11 +3,11 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import FormView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from .forms import HeroCreateForm, HeroUpgradeForm
+from .forms import HeroForm, HeroStatisticsForm
 from .models.hero import Hero
+from .models.item import Item
+from .models.statistic import HeroStatistic
 from .decorators import hero_created_require, logged_user_redirect_to_main_view
-from .models.new_hero import NewHero
-from .models.new_item import NewItem
 
 
 @method_decorator(logged_user_redirect_to_main_view, name='dispatch')
@@ -23,7 +23,7 @@ class MainView(TemplateView):
 @method_decorator(login_required, name='dispatch')
 class HeroCreateView(FormView):
     template_name = 'game/create_hero.html'
-    form_class = HeroCreateForm
+    form_class = HeroForm
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -40,23 +40,12 @@ class HeroCreateView(FormView):
 @method_decorator(hero_created_require, name='dispatch')
 class HeroUpgradeView(UpdateView):
     template_name = 'game/upgrade_hero.html'
-    form_class = HeroUpgradeForm
     model = Hero
 
     def get_object(self, queryset=None):
         # TODO how get current hero (if we would have more than one to choose)?
-        obj = Hero.objects.get(owner=self.request.user)
+        obj = Hero.objects.get(user=self.request.user)
         return obj
-
-    def get_initial(self):
-        initial = super(HeroUpgradeView, self).get_initial()
-        # TODO how get current hero (if we would have more than one to choose)?
-        hero = Hero.objects.get(owner=self.request.user)
-        initial['strength'] = hero.strength
-        initial['intelligence'] = hero.intelligence
-        initial['agility'] = hero.agility
-        initial['vitality'] = hero.vitality
-        return initial
 
     def form_valid(self, form):
         form.save()
@@ -64,3 +53,38 @@ class HeroUpgradeView(UpdateView):
 
     def get_success_url(self):
         return reverse('game:main')
+
+
+def create_hero(request):
+    if request.method == 'POST':
+        # TODO pass arguments to statistic and hero models
+        statistics = ['strength', 'intelligence', 'agility', 'vitality']
+        hero_form = HeroForm(request.POST)
+        hero_statistics_form = HeroStatisticsForm(request.POST)
+        if all([hero_form.is_valid(), hero_statistics_form.is_valid()]):
+            hero = Hero(
+                name = hero_form.cleaned_data['name'],
+                user = request.user
+            )
+            hero.save()
+            hero = Hero.objects.filter(user=request.user)[0] # reload
+
+            for statistic_name in statistics:
+                hero_statistic = HeroStatistic(
+                    name=statistic_name,
+                    owner=hero,
+                    value=hero_statistics_form.cleaned_data[statistic_name]
+                )
+                hero_statistic.save()
+
+
+        return render(request, 'game/success.html', {
+            'information': 'User created'
+        })
+    else:
+        hero_form = HeroForm()
+        hero_statistics_form = HeroStatisticsForm()
+        return render(request, 'game/create_hero.html', {
+            'hero_form': hero_form,
+            'hero_statistics_form': hero_statistics_form
+        })
