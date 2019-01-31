bc4244aa0e129a6cf2597b426846a0d6f9fe9d2c
goodude123 <assasinmcpl@gmail.com>
2019-01-31 14:17:59 +0100

    String representation methods tests.

--- game/tests/test_models/test_statistic.py
+++ game/tests/test_models/test_statistic.py
@@ -1,5 +1,6 @@
-from ...models.statistic import Statistic
+from ...models.statistic import Statistic, HeroStatistic, ItemStatistic
 from ...models.hero import Hero
+from ...models.item import Item
 from .set_ups import UserSetUp
 
 
@@ -49,3 +50,38 @@ class TestStatistic(UserSetUp):
         child_statistic.calculate_value()
         self.assertEqual(child_statistic.value, value*multiplier)
 
+
+class TestHeroStatistic(UserSetUp):
+    def test_hero_statistic_string_representation(self):
+        hero = Hero.objects.create(
+            user=self.user,
+            name='hero'
+        )
+        statistic_name = 'strength'
+        hero_statistic = HeroStatistic.objects.create(
+            name=statistic_name,
+            value=10,
+            owner=hero
+        )
+        string_representation = hero.name + ' statistic: ' + statistic_name
+        self.assertEqual(str(hero_statistic), string_representation)
+
+
+class TestItemStatistic(UserSetUp):
+    def test_item_statistic_string_representation(self):
+        hero = Hero.objects.create(
+            user=self.user,
+            name='hero'
+        )
+        item = Item.objects.create(
+            owner=hero,
+            name='item'
+        )
+        statistic_name = 'strength'
+        item_statistic = ItemStatistic.objects.create(
+            name=statistic_name,
+            value=10,
+            owner=item
+        )
+        string_representation = item.name + ' statistic: ' + statistic_name
+        self.assertEqual(str(item_statistic), string_representation)
