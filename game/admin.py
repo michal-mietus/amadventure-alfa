fcd4429cfe66020ced4c5e517c82b9e9c7b0f2e1
goodude123 <assasinmcpl@gmail.com>
2019-01-31 14:16:48 +0100

    Registered other views.

--- game/admin.py
+++ game/admin.py
@@ -4,6 +4,8 @@ from .models.item import Item
 from .models.location import Location
 from .models.action import Action
 from .models.statistic import Statistic
+from .models.statistic import HeroStatistic
+from .models.statistic import ItemStatistic
 
 
 admin.site.register(Hero)
@@ -11,3 +13,5 @@ admin.site.register(Item)
 admin.site.register(Location)
 admin.site.register(Action)
 admin.site.register(Statistic)
+admin.site.register(HeroStatistic)
+admin.site.register(ItemStatistic)
