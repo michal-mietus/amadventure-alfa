ee2cea756a1c9e64d11e31094bd59d634864d8b3
goodude123 <assasinmcpl@gmail.com>
2019-02-01 03:09:32 +0100

    CreateHeroView as CBV.

--- game/urls.py
+++ game/urls.py
@@ -7,7 +7,7 @@ app_name = 'game'
 urlpatterns = [
     path('', views.WelcomeView.as_view(), name='amadventure'),
     path('main/', views.MainView.as_view(), name='main'),
-    path('create_hero/', views.create_hero, name='create_hero'),
+    path('create_hero/', views.CreateHeroView.as_view(), name='create_hero'),
     path('upgrade_hero/', views.UpgradeHeroView.as_view(), name='upgrade_hero'),
     path('hero/<int:pk>/', views.HeroDetail.as_view(), name='hero_detail'),
 ]
