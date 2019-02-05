3e81b89524531e23085ed64bc8585e20c08da8d7
goodude123 <assasinmcpl@gmail.com>
2019-02-05 02:32:28 +0100

    UpgradeHeroView tests.

--- game/tests/test_views.py
+++ game/tests/test_views.py
@@ -1,7 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from ..models.hero import Hero
+from ..models.statistic import HeroStatistic
+from .. import views
 
 
 class UserSetUp(TestCase):
@@ -60,6 +62,7 @@ class TestHeroCreateView(UserSetUp):
 class TestHeroUpgradeView(UserSetUp):
     def setUp(self):
         super().setUp()
+        self.factory = RequestFactory()
         self.app_name = 'game'
         self.url_name = 'upgrade_hero'
         self.url = self.app_name + ':' + self.url_name
@@ -72,3 +75,55 @@ class TestHeroUpgradeView(UserSetUp):
         self.login_user()
         response = self.client.get(reverse(self.url))
         self.assertTemplateUsed('upgrade_hero.html')
+
+    def test_if_user_dont_have_hero_redirect_to_create_hero_view(self):
+        self.login_user()
+        response = self.client.get(reverse(self.url))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('game:create_hero'))
+
+    def test_form_valid_upgrade_hero(self):
+        self.login_user()
+        hero = Hero.objects.create(
+            name='hero name',
+            user=self.user
+        )
+        self.create_hero_statistics(hero)
+        statistics = {
+            'strength': 10,
+            'intelligence': 10,
+            'agility': 10,
+            'vitality': 10
+        }
+        response = self.client.post(reverse(self.url), statistics)
+        self.assertEqual(response.status_code, 302)
+        self.assertQuerysetEqual(
+            hero.get_all_statistics(), 
+            HeroStatistic.objects.filter(owner=hero),
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+    def test_upgrade_hero_view_initial_value(self):
+        hero = Hero.objects.create(
+            name='hero_name',
+            user=self.user
+        )
+        self.create_hero_statistics(hero)
+        request = self.factory.get(reverse('game:upgrade_hero'))
+        request.user = self.user
+        view = views.UpgradeHeroView()
+        view.request = request
+        initial_returned = view.get_initial()
+        statistic_names = ['strength', 'intelligence', 'agility', 'vitality']
+        for statistic_name in statistic_names:
+            self.assertEqual(initial_returned[statistic_name], 10)
+
+    def create_hero_statistics(self, hero):
+        statistic_names = ['strength', 'intelligence', 'agility', 'vitality']
+        for statistic_name in statistic_names:
+            HeroStatistic.objects.create(
+                owner=hero,
+                name=statistic_name,
+                value=10
+            )
