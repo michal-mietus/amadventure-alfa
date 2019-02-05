0b84872fde19fb20a63ea24d242b8051cdaba712
goodude123 <assasinmcpl@gmail.com>
2019-02-05 03:17:52 +0100

    Hero owned link and hero list view renamed url to heroes.

--- game/urls.py
+++ game/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('main/', views.MainView.as_view(), name='main'),
     path('create_hero/', views.CreateHeroView.as_view(), name='create_hero'),
     path('upgrade_hero/', views.UpgradeHeroView.as_view(), name='upgrade_hero'),
+    path('heroes/', views.HeroList.as_view(), name='hero_list'),
+    path('hero_owned/', views.HeroOwned.as_view(), name='hero_owned'),
     path('hero/<int:pk>/', views.HeroDetail.as_view(), name='hero_detail'),
 ]
