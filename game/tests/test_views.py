5164d29df79e07e614bfb0de08c1684bcbaaa250
goodude123 <assasinmcpl@gmail.com>
2019-02-08 09:39:39 +0100

    New tests for views:   - creating hero,   - creating derivative statics with hero,   - calculating new value of derivative statistics

--- game/tests/test_views.py
+++ game/tests/test_views.py
@@ -1,11 +1,13 @@
+from itertools import chain
 from django.test import TestCase, Client, RequestFactory
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from ..models.hero import Hero
-from ..models.statistic import HeroStatistic
+from ..models.statistics import HeroMainStatistic, HeroDerivativeStatistic
+from ..forms import MainStatisticsForm
+from ..statistic_properties import derivative_statistics
 from .. import views
-
 
 
 class UserSetUp(TestCase):
@@ -17,6 +19,7 @@ class UserSetUp(TestCase):
             password=self.password
         )
         self.client = Client()
+        self.factory = RequestFactory()
 
     def login_user(self):
         self.client.login(username=self.username, password=self.password)
@@ -53,12 +56,13 @@ class TestMainView(UserSetUp):
         self.assertTemplateUsed('main.html')
     
 
-class TestHeroCreateView(UserSetUp):
+class TestHeroCreateView(UserAndHeroSetUp):
     def setUp(self):
         super().setUp()
         self.app_name = 'game'
         self.url_name = 'create_hero'
         self.url = self.app_name + ':' + self.url_name
+        self.view = views.CreateHeroView()
 
     def test_prevent_logged_user_access_create_hero_view(self):
         response = self.client.get(reverse(self.url))
@@ -69,6 +73,39 @@ class TestHeroCreateView(UserSetUp):
         response = self.client.get(reverse(self.url))
         self.assertTemplateUsed('create_hero.html')
 
+    def test_create_hero_statistics_method(self):
+        data = {
+            'strength': 10,
+            'intelligence': 10,
+            'agility': 10,
+            'vitality': 10,
+        }
+        request = self.factory.post(reverse('game:create_hero'), data=data)
+        form = MainStatisticsForm(request.POST)
+        form.is_valid()
+        self.view.create_hero_statistics(hero=self.hero, statistics_form=form)
+        all_statistics = self.hero.heromainstatistic_set.all()
+        self.assertQuerysetEqual(
+            HeroMainStatistic.objects.all(), 
+            all_statistics,
+            transform=lambda x: x,
+            ordered=False
+        )
+
+    def test_create_derivative_statistics_method(self):
+        parent_statistic = HeroMainStatistic.objects.create(
+            name='strength',
+            value=10,
+            hero=self.hero
+        )
+        self.view.create_derivative_statistics(parent_statistic, self.hero)
+        statistic_count = 0
+        for statistic in derivative_statistics:
+            if statistic['parent_name'] == parent_statistic.name:
+                statistic_count += 1
+
+        self.assertEqual(len(HeroDerivativeStatistic.objects.all()), statistic_count)
+
 
 class TestUpgradeHeroView(UserSetUp):
     def setUp(self):
@@ -77,6 +114,7 @@ class TestUpgradeHeroView(UserSetUp):
         self.app_name = 'game'
         self.url_name = 'upgrade_hero'
         self.url = self.app_name + ':' + self.url_name
+        self.view = views.UpgradeHeroView()
     
     def test_prevent_logged_user_access_upgrade_view(self):
         response = self.client.get(reverse(self.url))
@@ -95,10 +133,7 @@ class TestUpgradeHeroView(UserSetUp):
 
     def test_form_valid_upgrade_hero(self):
         self.login_user()
-        hero = Hero.objects.create(
-            name='hero name',
-            user=self.user
-        )
+        hero = self.create_hero()
         self.create_hero_statistics(hero)
         statistics = {
             'strength': 10,
@@ -109,18 +144,35 @@ class TestUpgradeHeroView(UserSetUp):
 
         response = self.client.post(reverse(self.url), statistics)
         self.assertEqual(response.status_code, 302)
+        all_statistics = list(chain(HeroMainStatistic.objects.filter(hero=hero), HeroDerivativeStatistic.objects.filter(hero=hero)))
         self.assertQuerysetEqual(
             hero.get_all_statistics(), 
-            HeroStatistic.objects.filter(owner=hero),
+            HeroMainStatistic.objects.filter(hero=hero),
             transform=lambda x: x,
             ordered=False,
         )
+    
+    def test_calculate_new_derivative_statistic_values(self):
+        hero = self.create_hero()
+        create_hero_view = views.CreateHeroView()
+        parent_stat = HeroMainStatistic.objects.create(
+            name='strength',
+            value=10,
+            hero=hero
+        )
+        create_hero_view.create_derivative_statistics(parent_stat, hero)
+        value_after_upgrade = 20
+        parent_stat.value = value_after_upgrade
+        parent_stat.save()
+        self.view.calculate_new_derivative_statistic_values(parent_stat)
+        for derivative_statistic in derivative_statistics:
+            if derivative_statistic['parent_name'] == parent_stat.name:
+                der_stat = HeroDerivativeStatistic.objects.get(name=derivative_statistic['name'])
+                self.assertEqual(der_stat.value, value_after_upgrade * derivative_statistic['multiplier'])
+
 
     def test_upgrade_hero_view_initial_value(self):
-        hero = Hero.objects.create(
-            name='hero_name',
-            user=self.user
-        )
+        hero = self.create_hero()
         self.create_hero_statistics(hero)
         request = self.factory.get(reverse('game:upgrade_hero'))
         request.user = self.user
@@ -134,11 +186,17 @@ class TestUpgradeHeroView(UserSetUp):
     def create_hero_statistics(self, hero):
         statistic_names = ['strength', 'intelligence', 'agility', 'vitality']
         for statistic_name in statistic_names:
-            HeroStatistic.objects.create(
-                owner=hero,
+            HeroMainStatistic.objects.create(
+                hero=hero,
                 name=statistic_name,
                 value=10
             )
+
+    def create_hero(self):
+        return Hero.objects.create(
+            name='hero_name',
+            user=self.user
+        )
 
 
 class TestHeroOwnedView(UserSetUp):
@@ -170,9 +228,8 @@ class TestHeroOwnedView(UserSetUp):
     def create_hero_statistics(self, hero):
         statistic_names = ['strength', 'intelligence', 'agility', 'vitality']
         for statistic_name in statistic_names:
-            HeroStatistic.objects.create(
-                owner=hero,
+            HeroMainStatistic.objects.create(
+                hero=hero,
                 name=statistic_name,
                 value=10
             )
-
