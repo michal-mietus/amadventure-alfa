966e5466b831984976f85c3579e9c6d22edf8c4c
goodude123 <assasinmcpl@gmail.com>
2019-02-08 09:40:59 +0100

    String representation tests, calculating new value for new statistic models.

--- game/tests/test_models/test_statistic.py
+++ game/tests/test_models/test_statistic.py
@@ -1,21 +1,25 @@
-from ...models.statistic import Statistic, HeroStatistic, ItemStatistic
+from ...models.statistics import  HeroMainStatistic, ItemMainStatistic, HeroDerivativeStatistic, ItemDerivativeStatistic
 from ...models.hero import Hero
 from ...models.item import Item
 from .set_ups import UserSetUp
 
 
-class TestStatistic(UserSetUp):
+class TestHeroMainStatistic(UserSetUp):
     def setUp(self):
         super().setUp()
-        self.statistic = Statistic.objects.create(
-            name='strength',
-            multiplier=1,
-            value=10,
+        self.hero = Hero.objects.create(
+            name='hero_name',
+            user=self.user
         )
 
     def test_statistic_string_representation(self):
-        str_repr = "Statistic " + self.statistic.name
-        self.assertEqual(str(self.statistic), str_repr)
+        statistic = HeroMainStatistic.objects.create(
+            hero=self.hero,
+            name='statistic_name',
+            value=10,
+        )
+        str_repr = "Main statistic " + statistic.name
+        self.assertEqual(str(statistic), str_repr)
 
     def test_create_statistics(self):
         statistics_pairs = {
@@ -25,49 +29,72 @@ class TestStatistic(UserSetUp):
             'Vitality': 6,
         }
         for name, value in statistics_pairs.items():
-            Statistic.objects.create(
+            HeroMainStatistic.objects.create(
+                hero=self.hero,
                 name=name,
                 value=value,
             )
 
-    def test_create_child_statistic(self):
-        child_statistic = Statistic.objects.create(
-            name='defense',
-            parent=self.statistic,
-            multiplier=3,
-            value=10,
+
+class TestDerivativeStatistic(UserSetUp):
+    def setUp(self):
+        super().setUp()
+        self.hero = Hero.objects.create(
+            name='hero_name',
+            user=self.user,
         )
+        self.main_statistic = HeroMainStatistic.objects.create(
+            name='strength',
+            value=10,
+            hero=self.hero,
+        )
+        self.derivative_statistic = HeroDerivativeStatistic.objects.create(
+            name='attack',
+            multiplier=2,
+            hero=self.hero,
+            parent=self.main_statistic,
+        )
+
+    def test_statistic_string_representation(self):
+        str_repr = "Hero derivative statistic " + self.derivative_statistic.name
+        self.assertEqual(str(self.derivative_statistic), str_repr)
 
     def test_calculate_value(self):
-        value=10
-        multiplier=3
-        child_statistic = Statistic.objects.create(
-            name='defense',
-            parent=self.statistic,
-            multiplier=multiplier,
-            value=value,
-        )
-        child_statistic.calculate_value()
-        self.assertEqual(child_statistic.value, value*multiplier)
+        self.derivative_statistic.calculate_and_save_value()
+        value = self.derivative_statistic.parent.value * self.derivative_statistic.multiplier
+        self.assertEqual(self.derivative_statistic.value, value)
 
 
-class TestHeroStatistic(UserSetUp):
-    def test_hero_statistic_string_representation(self):
-        hero = Hero.objects.create(
+class TestHeroDerivativeStatistic(UserSetUp):
+    def setUp(self):
+        super().setUp()
+        self.hero = Hero.objects.create(
+            name='hero_name',
             user=self.user,
-            name='hero'
         )
-        statistic_name = 'strength'
-        hero_statistic = HeroStatistic.objects.create(
-            name=statistic_name,
+        self.main_statistic = HeroMainStatistic.objects.create(
+            name='strength',
             value=10,
-            owner=hero
+            hero=self.hero,
         )
-        string_representation = hero.name + ' statistic: ' + statistic_name
-        self.assertEqual(str(hero_statistic), string_representation)
+        self.derivative_statistic = HeroDerivativeStatistic.objects.create(
+            name='attack',
+            multiplier=2,
+            hero=self.hero,
+            parent=self.main_statistic,
+        )
+
+    def test_statistic_string_representation(self):
+        str_repr = "Hero derivative statistic " + self.derivative_statistic.name
+        self.assertEqual(str(self.derivative_statistic), str_repr)
+
+    def test_calculate_value(self):
+        self.derivative_statistic.calculate_and_save_value()
+        value = self.derivative_statistic.parent.value * self.derivative_statistic.multiplier
+        self.assertEqual(self.derivative_statistic.value, value)
 
 
-class TestItemStatistic(UserSetUp):
+class TestItemMainStatistic(UserSetUp):
     def test_item_statistic_string_representation(self):
         hero = Hero.objects.create(
             user=self.user,
@@ -78,10 +105,10 @@ class TestItemStatistic(UserSetUp):
             name='item'
         )
         statistic_name = 'strength'
-        item_statistic = ItemStatistic.objects.create(
+        item_statistic = ItemMainStatistic.objects.create(
             name=statistic_name,
             value=10,
-            owner=item
+            item=item
         )
-        string_representation = item.name + ' statistic: ' + statistic_name
+        string_representation = 'Main statistic ' + statistic_name
         self.assertEqual(str(item_statistic), string_representation)
