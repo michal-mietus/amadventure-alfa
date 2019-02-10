c65807463aa9cf82e1ba4aae5051e425c99b222c
goodude123 <assasinmcpl@gmail.com>
2019-02-10 03:29:06 +0100

    Removed unnecessary main view. Used instead old amadventure.

--- game/urls.py
+++ game/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'game'
 
 urlpatterns = [
-    path('', views.WelcomeView.as_view(), name='amadventure'),
-    path('main/', views.MainView.as_view(), name='main'),
+    path('', views.WelcomeView.as_view(), name='main'),
     path('create_hero/', views.CreateHeroView.as_view(), name='create_hero'),
     path('upgrade_hero/', views.UpgradeHeroView.as_view(), name='upgrade_hero'),
     path('heroes/', views.HeroList.as_view(), name='hero_list'),
