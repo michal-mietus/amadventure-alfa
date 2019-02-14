1b040720d79a2f97145eacae2e7f9773a5b27ab3
goodude123 <assasinmcpl@gmail.com>
2019-02-14 15:18:36 +0100

    Refactored all urls to be suburls from /hero/.

--- game/urls.py
+++ game/urls.py
@@ -6,10 +6,11 @@ app_name = 'game'
 
 urlpatterns = [
     path('', views.WelcomeView.as_view(), name='main'),
-    path('create_hero/', views.CreateHeroView.as_view(), name='create_hero'),
-    path('upgrade_hero/', views.UpgradeHeroView.as_view(), name='upgrade_hero'),
-    path('heroes/', views.HeroList.as_view(), name='hero_list'),
-    path('hero_owned/', views.HeroOwned.as_view(), name='hero_owned'),
+    path('hero/create/', views.CreateHeroView.as_view(), name='create_hero'),
+    path('hero/upgrade/', views.UpgradeHeroView.as_view(), name='upgrade_hero'),
+    path('hero/list/', views.HeroList.as_view(), name='hero_list'),
+    path('hero/owned/', views.HeroOwned.as_view(), name='hero_owned'),
     path('hero/<int:pk>/', views.HeroDetail.as_view(), name='hero_detail'),
-    path('hero/fight/<int:defender_pk>', views.FightView.as_view(), name='hero_fight'),
+    path('hero/fight/<int:defender_pk>/', views.FightView.as_view(), name='hero_fight'),
+    path('hero/delete/<int:pk>/', views.HeroDeleteView.as_view(), name='hero_delete'),
 ]
