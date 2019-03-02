0f66c24d8c2587d0cecea64c7d97d8f1f0185b90
goodude123 <assasinmcpl@gmail.com>
2019-03-02 14:19:13 +0100

    Added two fields: description, difficulty.

--- game/models/location.py
+++ game/models/location.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField()
+    difficulty = models.IntegerField(max_length=20) # range(3-7)
     
     def __str__(self):
         return "Location " + self.name
